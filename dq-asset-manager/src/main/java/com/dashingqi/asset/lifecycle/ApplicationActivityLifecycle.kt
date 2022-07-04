23ff52dd0cdf5b23fd8a346b3872a10d9d826cd2
zhangqi61 <zhangqi61@baidu.com>
2022-07-04 19:30:38 +0800

    【update】

--- dq-asset-manager/src/main/java/com/dashingqi/asset/lifecycle/ApplicationActivityLifecycle.kt
+++ dq-asset-manager/src/main/java/com/dashingqi/asset/lifecycle/ApplicationActivityLifecycle.kt
@@ -10,17 +10,17 @@ import android.os.Bundle
  * @since 2022/6/23
  */
 open  class ApplicationActivityLifecycle : Application.ActivityLifecycleCallbacks {
-    override fun onActivityCreated(p0: Activity, p1: Bundle?) {}
+    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {}
 
-    override fun onActivityStarted(p0: Activity) {}
+    override fun onActivityStarted(activity: Activity) {}
 
-    override fun onActivityResumed(p0: Activity) {}
+    override fun onActivityResumed(activity: Activity) {}
 
-    override fun onActivityPaused(p0: Activity) {}
+    override fun onActivityPaused(activity: Activity) {}
 
-    override fun onActivityStopped(p0: Activity) {}
+    override fun onActivityStopped(activity: Activity) {}
 
-    override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {}
+    override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {}
 
-    override fun onActivityDestroyed(p0: Activity) {}
+    override fun onActivityDestroyed(activity: Activity) {}
 }
